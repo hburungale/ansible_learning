d3a1ad034c960fb62e61cb616fdf488c2e8b0aee
hburungale <hburungale@mdsol.com>
2024-01-19 11:26:45 +0530

    ad

--- uploadingJira.py
+++ uploadingJira.py
@@ -1,10 +1,14 @@
 import requests
+import os
 
 def upload_attachment():
     url = "https://jira.mdsol.com/rest/api/2/issue/IH-454862/attachments"
     username = "sdteam07devops"
     password = "\$4FQ89NXts\$IgJ9O"
-    file_path = "../reports/IR_automation.py"
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the correct file path
+    file_path = os.path.join(script_directory, "../reports/IR_automation.py")
     
     # Prepare the authentication credentials
     auth = (username, password)
