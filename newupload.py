f6a38f9d4e7fa80b609f6f1ddc32b9f365eadd6b
hburungale <hburungale@mdsol.com>
2024-01-27 00:09:52 +0530

    python script

--- newupload.py
+++ newupload.py
@@ -2,22 +2,17 @@ import requests
 import os
 
 def upload_attachment():
-    url = "https://jira.mdsol.com/rest/api/2/issue/IH-454862/attachments"
-    access_token = os.environ.get('Bearer')
+    access_token = os.environ.get('Access_Token')
+    ticket_id = os.environ.get('Jira_Ticket_Id')
+    url = f"https://jira.mdsol.com/rest/api/2/issue/{ticket_id}/attachments"
     script_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_directory, "../reports/IR_automation.py")
     headers = {
     'X-Atlassian-Token': 'nocheck',
     'Authorization': f'Bearer {access_token}'
     }
-
-    # Prepare the file payload
     files = {"file": open(file_path, "rb")}
-
-    # Make the HTTP POST request
     response = requests.post(url, headers=headers, files=files)
-
-    # Print the response
     print(response.status_code)
     print(response.text)
 
