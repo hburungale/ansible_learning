2b428b1152c8b4541391e953da0381125ab85760
hburungale <hburungale@mdsol.com>
2024-01-30 16:59:40 +0530

    adds

--- wordfile_update.py
+++ wordfile_update.py
@@ -46,11 +46,11 @@ if __name__ == "__main__":
     # Specify the path to the Word file
     script_directory = os.path.dirname(os.path.abspath(__file__))
     print(script_directory)
-    file_path = os.path.join(script_directory, "./app_codes/CTMS_Val1_PIR.docx")
-    print(file_path)
-    file_path1= "./app_codes/CTMS_Val1_PIR.docx"
-    print(file_path1)
-    file_path2 = os.path.join(script_directory, "/app_codes/CTMS_Val1_PIR.docx")
+    # file_path = os.path.join(script_directory, "./app_codes/CTMS_Val1_PIR.docx")
+    # print(file_path)
+    # file_path1= "./app_codes/CTMS_Val1_PIR.docx"
+    # print(file_path1)
+    file_path2 = os.path.join(script_directory, "/CTMS_Val1_PIR.docx")
     print(file_path2)
 
 
