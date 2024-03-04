02cb30722a9ce60aca64ff8b06f1053047b0549b
hburungale <hburungale@mdsol.com>
2024-03-04 16:54:35 +0530

    ch

--- finalfile2.py
+++ finalfile2.py
@@ -1,5 +1,5 @@
 import requests
-import json
+#import json
 import os
 from docx import Document
 from datetime import datetime
@@ -8,12 +8,16 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
 
+#Global variables
+cyclops_template = os.environ.get('TEMPLATE')
+CTMS_URL = os.environ.get('CTMS_URL')
+S3 =os.environ.get('S3')
 
 #            *** To fetch the data form cyclops ***
 def get_json_data():
-    cyclops_template = os.environ.get('TEMPLATE')
-    CTMS_URL = os.environ.get('CTMS_URL')
-    S3 =os.environ.get('S3')
+    # cyclops_template = os.environ.get('TEMPLATE')
+    # CTMS_URL = os.environ.get('CTMS_URL')
+    # S3 =os.environ.get('S3')
     if S3 == "red":
      cyclops_url = "https://cyclopsui.imedidata.com/"
     else:
@@ -32,7 +36,6 @@ def get_json_data():
         print(f"Error: {response.status_code} - {response.text}")
         return None
 
-
 #             *** To update the PIR file ***
 def replace_word_file_content():
     json_data = get_json_data()
@@ -40,7 +43,7 @@ def replace_word_file_content():
     now = datetime.now()
     deploy_date= now.strftime("%d-%m-%Y")
     print("Current Working Directory:", current_directory)
-    file_name="CTMS_PIR.docx"
+    file_name="CTMS_PIR.docx" #add in env variables 
     word_file_path = os.path.join(current_directory, file_name)
     print("word_file_path:" , word_file_path)
     replacements = {
@@ -51,13 +54,13 @@ def replace_word_file_content():
         "Deploy_Date":deploy_date
     }
     document = Document(file_name)
-    print("Before Replacement:")
-    for table in document.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                print(cell.text)
+    # print("Before Replacement:")
+    # for table in document.tables:
+    #     for row in table.rows:
+    #         for cell in row.cells:
+    #             print(cell.text)
                 
-    
+    # try to optimize this
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
@@ -74,7 +77,7 @@ def replace_word_file_content():
     output_path = "PIR.docx"
     document.save(output_path)
     print(f"\nUpdated Word file saved to: {output_path}")
-    pdfconverter('PIR.docx')
+    pdfconverter(output_path)
 
 
 
@@ -105,9 +108,9 @@ def pdfconverter(docx_path):
     doc = Document(docx_path)
     table_count = 0
     # To create PDF file
-    pdfname=os.environ.get('CTMS_URL')
-    pdffilename = f"{pdfname}_PIR.pdf"
-    pdf = SimpleDocTemplate(pdffilename, pagesize=letter)
+    # pdfname=os.environ.get('CTMS_URL')
+    pdffilename = f"{CTMS_URL}_PIR.pdf"
+    pdf = SimpleDocTemplate(pdffilename, pagesize=letter) #find 
     pdf_tables = []
     for table_index, table in enumerate(doc.tables):
         table_count += 1   
@@ -130,8 +133,8 @@ def pdfconverter(docx_path):
 
 #    *** To copy PIR.pdf to the reports directory ***
 def copyfile():
-    url=os.environ.get('CTMS_URL')
-    pirfilename = f"{url}_PIR.pdf" 
+    # url=os.environ.get('CTMS_URL')
+    pirfilename = f"{CTMS_URL}_PIR.pdf" 
     destination_folder = "./reports/"
     destination_path = os.path.join(destination_folder, pirfilename)
     shutil.copy(pirfilename, destination_path)
@@ -143,12 +146,13 @@ def copyfile():
 def upload_attachment():
     access_token = os.environ.get('Access_Token')
     ticket_id = os.environ.get('Jira_Ticket_Id')
-    url=os.environ.get('CTMS_URL')
+    # url=os.environ.get('CTMS_URL')
     json_data = get_json_data()
     Git_Branch = json_data["global"]["GIT_BRANCH"]
-    pirfilename = f"{url}_PIR.pdf"
-    # irfilename = f"{url}_{Git_Branch}.pdf" 
+    pirfilename = f"{CTMS_URL}_PIR.pdf"
+    # irfilename = f"{CTMS_URL}_{Git_Branch}.pdf" 
     url = f"https://jira.mdsol.com/rest/api/2/issue/{ticket_id}/attachments"
+
     script_directory = os.path.dirname(os.path.abspath(__file__))
     # file_path = os.path.join(script_directory, f"./reports/{irfilename}")
     file_path = os.path.join(script_directory, f"./reports/{pirfilename}")
