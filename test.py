ad5bdd14c51e543c0b5e46c0dd812e6f916e6f9b
hburungale <hburungale@mdsol.com>
2024-03-05 14:17:44 +0530

    ch

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-import json
+#import json
 import os
 from docx import Document
 from datetime import datetime
@@ -8,17 +8,20 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
 
+#Global variables
+CTMS_URL = "url"
 
 #            *** To fetch the data form cyclops ***
 def get_json_data():
     cyclops_template = os.environ.get('TEMPLATE')
-    CTMS_URL = os.environ.get('CTMS_URL')
     S3 =os.environ.get('S3')
     if S3 == "red":
      cyclops_url = "https://cyclopsui.imedidata.com/"
     else:
       cyclops_url = "https://cyclopsui-sandbox.imedidata.net"
-    url = f"{cyclops_url}/api/v0/url-configurations/CTMS/{cyclops_template}/{CTMS_URL}"
+    # url = f"{cyclops_url}/api/v0/url-configurations/CTMS/{cyclops_template}/{CTMS_URL}"
+    url = "https://cyclopsui-sandbox.imedidata.net/api/v0/url-configurations/CTMS/CTMS_DIstro/test_url.net"
+
     payload = {}
     headers = {
         'Access-Token': os.environ.get('cyclops'), #stored in secure variables, will be adding in secrete manager after testing
@@ -32,51 +35,37 @@ def get_json_data():
         print(f"Error: {response.status_code} - {response.text}")
         return None
 
-
 #             *** To update the PIR file ***
 def replace_word_file_content():
     json_data = get_json_data()
-    current_directory = os.getcwd()
-    now = datetime.now()
-    deploy_date= now.strftime("%d-%m-%Y")
-    print("Current Working Directory:", current_directory)
-    file_name="CTMS_PIR.docx"
-    word_file_path = os.path.join(current_directory, file_name)
-    print("word_file_path:" , word_file_path)
+    deploy_time = datetime.now()
+    deploy_date= deploy_time.strftime("%d-%m-%Y")
+    file_name= os.environ.get('PIR_template') 
     replacements = {
-        # "Product_Version": json_data["global"]["CTMS_VERSION"],
-        "Environment_URL": os.environ.get('CTMS_URL') ,
-        "Git_Branch": json_data["global"]["GIT_BRANCH"],
-        "Deploy_By": json_data["global"]["deploy_by"],
-        "Deploy_Date":deploy_date
+        "Product_Version": "p",
+        "Environment_URL": "CTMS_URL",
+        "Git_Branch": "g",
+        "Deploy_By": "d",
+        "Deploy_Date":"d"
     }
     document = Document(file_name)
-    print("Before Replacement:")
-    for table in document.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                print(cell.text)
-                
-    
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
                 for target_word, replacement_word in replacements.items():
                     cell.text = cell.text.replace(target_word, replacement_word)
 
-    print("\nAfter Replacement:")
-    for table in document.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                print(cell.text)
+    # print("\nAfter Replacement:")
+    # for table in document.tables:
+    #     for row in table.rows:
+    #         for cell in row.cells:
+    #             print(cell.text)
 
     # Save the updated Word document
     output_path = "PIR.docx"
     document.save(output_path)
     print(f"\nUpdated Word file saved to: {output_path}")
-    pdfconvertor(output_path)
-
-
+    pdfconverter(output_path)
 
 #             *** To convert the PIR word into pdf format ***
 def apply_styles(pdf_table, table_index):
@@ -99,13 +88,13 @@ def apply_styles(pdf_table, table_index):
 
     pdf_table.setStyle(style)
 
-def pdfconvertor(docx_path):
+def pdfconverter(docx_path):
     doc = Document(docx_path)
     table_count = 0
     # To create PDF file
-    pdfname=os.environ.get('CTMS_URL')
-    pdffilename = f"{pdfname}_PIR.pdf"
-    pdf = SimpleDocTemplate(pdffilename, pagesize=letter)
+    pdffilename = f"{CTMS_URL}_PIR.pdf"
+    #to create a pdf template by passing the name and size of the file.
+    pdf = SimpleDocTemplate(pdffilename, pagesize=letter) #find 
     pdf_tables = []
     for table_index, table in enumerate(doc.tables):
         table_count += 1   
@@ -122,28 +111,32 @@ def pdfconvertor(docx_path):
         pdf_tables.append(pdf_table)
     # Build the PDF file with all tables
     pdf.build(pdf_tables)
+    copyfile()  
+
 
 
 #    *** To copy PIR.pdf to the reports directory ***
 def copyfile():
-    url=os.environ.get('CTMS_URL')
-    pirfilename = f"{url}_PIR.pdf" 
+    # url=os.environ.get('CTMS_URL')
+    pirfilename = f"{CTMS_URL}_PIR.pdf" 
     destination_folder = "./reports/"
     destination_path = os.path.join(destination_folder, pirfilename)
     shutil.copy(pirfilename, destination_path)
     upload_attachment() 
 
 
+
 #   *** To upload IR and PIR to the jira ticket
 def upload_attachment():
     access_token = os.environ.get('Access_Token')
     ticket_id = os.environ.get('Jira_Ticket_Id')
-    url=os.environ.get('CTMS_URL')
+    # url=os.environ.get('CTMS_URL')
     json_data = get_json_data()
     Git_Branch = json_data["global"]["GIT_BRANCH"]
-    pirfilename = f"{url}_PIR.pdf"
-    # irfilename = f"{url}_{Git_Branch}.pdf" 
+    pirfilename = f"{CTMS_URL}_PIR.pdf"
+    # irfilename = f"{CTMS_URL}_{Git_Branch}.pdf" 
     url = f"https://jira.mdsol.com/rest/api/2/issue/{ticket_id}/attachments"
+
     script_directory = os.path.dirname(os.path.abspath(__file__))
     # file_path = os.path.join(script_directory, f"./reports/{irfilename}")
     file_path = os.path.join(script_directory, f"./reports/{pirfilename}")
@@ -160,6 +153,7 @@ def upload_attachment():
 
 #execution starts from here
 replace_word_file_content()                     # 1. To call replacement function
-copyfile()                                      # 2. To copy PIR.pdf to the reports directory
-                           
+#wordfile_to_pdffile = pdfconverter('PIR.docx')  # 2. To convert wordfile to pdf file
+# copyfile()                                      # 3. To copy PIR.pdf to the reports directory
+# upload_attachment()                             # 4. To upload IR and PIR to the jira ticket
 
