222981a24b5c77f8ac8a103ec391c90c3fddcbc2
hburungale <hburungale@mdsol.com>
2024-03-07 18:38:20 +0530

    changes

--- fetchaws.py
+++ fetchaws.py
@@ -1,7 +1,15 @@
 import boto3
+import os 
 def fetch_token(token_name):
    ssm=boto3.client('ssm', region_name='us-east-1')
    parameter=ssm.get_parameter(Name=token_name, WithDecryption =True)
    AUTH_TOKEN=parameter['Parameter']['Value']
-   return AUTH_TOKEN
-print(fetch_token("/Automating/Deployments/CTMS/Cyclops/Accesstoken")) 
+   print(AUTH_TOKEN)
+a= os.environ.get('cyclops')
+b=os.environ.get('Access_Token')
+print(a,b)  
+fetch_token("jira_token_devops")
+print(fetch_token("jira_token_devops"))
+print(fetch_token("/Automating/Deployments/CTMS/Cyclops/Accesstoken"))
+
+
