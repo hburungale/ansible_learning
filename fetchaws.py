f1e3fc3d8850a97d98c158bda16a2575d4d959d9
hburungale <hburungale@mdsol.com>
2024-03-07 21:36:25 +0530

    added jira

--- fetchaws.py
+++ fetchaws.py
@@ -4,12 +4,14 @@ def fetch_token(token_name):
    ssm=boto3.client('ssm', region_name='us-east-1')
    parameter=ssm.get_parameter(Name=token_name, WithDecryption =True)
    AUTH_TOKEN=parameter['Parameter']['Value']
-   print(AUTH_TOKEN)
-a= os.environ.get('cyclops')
-b=os.environ.get('Access_Token')
-print(a,b)  
-fetch_token(a)
-fetch_token(b)
+   return AUTH_TOKEN
+# a= os.environ.get('cyclops')
+b="jira_token_devops"
+if fetch_token(b):
+   print("token found")
+else:
+   print("token found")
+
 
 
 
