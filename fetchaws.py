0c68301c1fd3253375cb852851344681ca5042d6
hburungale <hburungale@mdsol.com>
2024-03-07 22:52:54 +0530

    add

--- fetchaws.py
+++ fetchaws.py
@@ -1,16 +1,18 @@
 import boto3
-import os 
+import os
+JIRA_TOKEN_NAME="jira_token_devops" 
 def fetch_token(token_name):
    ssm=boto3.client('ssm', region_name='us-east-1')
    parameter=ssm.get_parameter(Name=token_name, WithDecryption =True)
    AUTH_TOKEN=parameter['Parameter']['Value']
    return AUTH_TOKEN
-# a= os.environ.get('cyclops')
-b="jira_token_devops"
-if fetch_token(b):
-   print("token found")
-else:
-   print("token found")
+def create_header(token_name):
+   access_Token=fetch_token(token_name)
+   return access_Token   
+
+token_name=JIRA_TOKEN_NAME
+header=create_header(token_name)
+print(header)
 
 
 
